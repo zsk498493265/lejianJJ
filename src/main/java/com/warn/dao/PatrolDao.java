ea328116daf83b09adc617f50863a593ae70b533
typeyuki <1462878291@qq.com>
2019-02-17 14:26:22 +0800

    2/17

--- src/main/java/com/warn/dao/PatrolDao.java
+++ src/main/java/com/warn/dao/PatrolDao.java
@@ -3,10 +3,16 @@ package com.warn.dao;
 import com.warn.entity.Cookie;
 import com.warn.entity.Patrol;
 
+import java.util.List;
+
 public interface PatrolDao {
       void addPatrolRecords(Patrol patrol);
 
       Cookie getCookie();
 
       Patrol getLatestRecord();
+
+      List<Patrol> getAllRecords();
+
+      Integer getOldIdByRecord(Integer point);
 }
